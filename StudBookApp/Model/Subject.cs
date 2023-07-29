62281ab83d8f165f9ca6a7fef153c0c9f01025be
Anton <anton260903@gmail.com>
2023-07-29 21:49:12 +0300

    grade property added to subject class

--- StudBookApp/Model/Subject.cs
+++ StudBookApp/Model/Subject.cs
@@ -6,8 +6,9 @@ namespace StudBookApp.Model
     {
         public string Name { get; set; }
         public double Credits { get; set; }
+        public int Grade { get; set; }
 
-        public Subject(string name, double creadits = 0.0)
+        public Subject(string name, double creadits = 0.0, int grade = 0)
         {
             if (creadits < 0 || creadits > 10)
             {
@@ -16,6 +17,7 @@ namespace StudBookApp.Model
 
             Name = name;
             Credits = creadits;
+            Grade = grade;
         }
     }
 }
