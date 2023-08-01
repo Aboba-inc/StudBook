fd4d41647c1a43ec956b9c2037add868aa243961
Anton <anton260903@gmail.com>
2023-08-01 23:57:27 +0300

    grade validation fixed to accept 0

--- StudBookApp/Views/CustomControls/SubjectField.axaml.cs
+++ StudBookApp/Views/CustomControls/SubjectField.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Media;
 using System;
 
@@ -10,6 +11,7 @@ namespace StudBookApp.Views.CustomControls
     public class SubjectField : TemplatedControl
     {
         TextBox? _gradeTextBox;
+        TextBox? _creditsTextBox;
 
         public static readonly StyledProperty<IBrush> BackgroundSubjectProperty =
         AvaloniaProperty.Register<SubjectField, IBrush>(nameof(BackgroundSubject), Brushes.White);
@@ -72,32 +74,66 @@ namespace StudBookApp.Views.CustomControls
             // if we had a control template before, we need to unsubscribe any event listeners
             if (_gradeTextBox is not null)
             {
-                _gradeTextBox.KeyDown -= OnGradeTextBox_KeyDown;
-                _gradeTextBox.PastingFromClipboard -= OnGrateTextBox_PastingFromClipboard;
-                _gradeTextBox.TextChanged -= OnGradeTextBox_TextChanged;
+                _gradeTextBox.KeyDown -= GradeTextBox_KeyDown;
+                _gradeTextBox.PastingFromClipboard -= GrateTextBox_PastingFromClipboard;
+                _gradeTextBox.TextChanged -= GradeTextBox_TextChanged;
+            }
+
+            if (_creditsTextBox is not null)
+            {
+                _creditsTextBox.KeyDown -= CreditsTextBox_KeyDown;
+                _creditsTextBox.PastingFromClipboard -= CreditsTextBox_PastingFromClipboard;
+                _creditsTextBox.TextChanged -= CreditsTextBox_TextChanged;
             }
 
             // try to find the control with the given name
             _gradeTextBox = e.NameScope.Find("GradeTextBox") as TextBox;
 
             // listen to pointer-released events on the stars presenter.
-            if (_gradeTextBox != null)
+            if (_gradeTextBox is not null)
             {
-                _gradeTextBox.KeyDown += OnGradeTextBox_KeyDown;
-                _gradeTextBox.PastingFromClipboard += OnGrateTextBox_PastingFromClipboard;
-                _gradeTextBox.TextChanged += OnGradeTextBox_TextChanged;
+                _gradeTextBox.KeyDown += GradeTextBox_KeyDown;
+                _gradeTextBox.PastingFromClipboard += GrateTextBox_PastingFromClipboard;
+                _gradeTextBox.TextChanged += GradeTextBox_TextChanged;
+            }
+
+            if (_creditsTextBox is not null)
+            {
+                _creditsTextBox.KeyDown += CreditsTextBox_KeyDown;
+                _creditsTextBox.PastingFromClipboard += CreditsTextBox_PastingFromClipboard;
+                _creditsTextBox.TextChanged += CreditsTextBox_TextChanged;
             }
         }
 
-        private void OnGradeTextBox_TextChanged(object? sender, TextChangedEventArgs e)
+        private void CreditsTextBox_TextChanged(object? sender, TextChangedEventArgs e)
         {
-            if (_gradeTextBox is not null && int.TryParse(_gradeTextBox.Text, out int grade) && grade == 0)
+            
+        }
+
+        private void CreditsTextBox_PastingFromClipboard(object? sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void CreditsTextBox_KeyDown(object? sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void GradeTextBox_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (_gradeTextBox?.Text == "00")
             {
-                _gradeTextBox.Text = string.Empty;
+                _gradeTextBox.Text = "0";
+            }
+
+            if (_gradeTextBox is not null && _gradeTextBox.Text?.Length > 1 && _gradeTextBox.Text[0] == '0')
+            {
+                _gradeTextBox.Text = _gradeTextBox.Text.TrimStart('0');
             }
         }
 
-        private void OnGradeTextBox_KeyDown(object? sender, KeyEventArgs e)
+        private void GradeTextBox_KeyDown(object? sender, KeyEventArgs e)
         {
             string key = e.Key.ToString();
 
@@ -109,20 +145,22 @@ namespace StudBookApp.Views.CustomControls
                 || !Char.IsDigit(key[1])
                 || (_gradeTextBox.SelectionStart == _gradeTextBox.SelectionEnd && int.TryParse(_gradeTextBox.Text.Insert(Math.Min(_gradeTextBox.SelectionStart, _gradeTextBox.SelectionEnd), key[1].ToString()), out int grade1) && grade1 > 100)
                 || (_gradeTextBox!.SelectionStart != _gradeTextBox.SelectionEnd && int.TryParse(_gradeTextBox.Text.Remove(Math.Min(_gradeTextBox.SelectionStart, _gradeTextBox.SelectionEnd), Math.Abs(_gradeTextBox.SelectionEnd - _gradeTextBox.SelectionStart)).Insert(Math.Min(_gradeTextBox.SelectionStart, _gradeTextBox.SelectionEnd), key[1].ToString()), out int grade2) && grade2 > 100)
-                || (Math.Min(_gradeTextBox.SelectionStart, _gradeTextBox.SelectionEnd) == 0 && key[1] == '0')
-                || (string.IsNullOrEmpty(_gradeTextBox.Text) && int.Parse(key[1].ToString()) == 0))
+                || (_gradeTextBox.Text.Length > 0 && Math.Min(_gradeTextBox.SelectionStart, _gradeTextBox.SelectionEnd) == 0 && key[1] == '0'))
             {
                 e.Handled = true;
             }
         }
 
-        private void OnGrateTextBox_PastingFromClipboard(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private void GrateTextBox_PastingFromClipboard(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             string? text = TopLevel.GetTopLevel(this)?.Clipboard?.GetTextAsync().Result;
-            if (!int.TryParse(_gradeTextBox?.Text + text, out int grade) || grade > 100 || grade < 0
+            if (!int.TryParse(_gradeTextBox?.Text + text, out int grade)
+                || grade > 100
+                || grade < 0
                 || (_gradeTextBox?.Text?.Length == 0 && grade == 0))
+            {
                 e.Handled = true;
+            }
         }
-
     }
 }
