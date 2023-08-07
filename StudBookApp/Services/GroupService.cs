16ebc0ce193bbd25b230eb626017a76d80d07b98
Anton <anton260903@gmail.com>
2023-08-07 21:11:49 +0300

    group service added

---
+++ StudBookApp/Services/GroupService.cs
@@ -0,0 +1,30 @@
+﻿using StudBookApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StudBookApp.Services
+{
+    public class GroupService
+    {
+        readonly string _projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
+        public async Task<IEnumerable<Group>> GetGroups()
+        {
+            var dataFile = $"groups.json";
+            List<Group> items;
+
+            //read data
+            using (var sr = new StreamReader(Path.Combine(_projectDirectory, dataFile)))
+            {
+                var json = sr.ReadToEnd();
+                items = JsonSerializer.Deserialize<List<Group>>(json);
+            }
+
+            return items;
+        }
+    }
+}
