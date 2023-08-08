00cd3d6502d5f0574d9926bb0299977664132f74
Anton <anton260903@gmail.com>
2023-08-08 16:36:12 +0300

    added faculty and course to group class

--- StudBookApp/Models/Group.cs
+++ StudBookApp/Models/Group.cs
@@ -9,12 +9,23 @@ namespace StudBookApp.Models
     public class Group
     {
         public string Name { get; set; }
+        public int Faculty { get; set; }
+        public int Course { get; set; }
         public List<Subject> Subjects { get; set; }
 
-        public Group(string name = "", List<Subject>? subjects = null)
+        public Group(string name = "", int fac = 1, int course = 1, List<Subject>? subjects = null)
         {
             Name = name;
+            Faculty = fac;
+            Course = course;
             Subjects = subjects ?? new List<Subject>();
+        }
+
+        public Group() { }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
